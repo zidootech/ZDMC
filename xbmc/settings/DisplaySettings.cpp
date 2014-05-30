5a2763bd5aa0d3082fe71021dc7ae410e769cf15
popcornmix <popcornmix@gmail.com>
2014-05-30 14:58:43 +0100

    [settings] Experiment: Report DESKTOP resolution in video settings

--- xbmc/settings/DisplaySettings.cpp
+++ xbmc/settings/DisplaySettings.cpp
@@ -704,6 +704,9 @@ void CDisplaySettings::SettingOptionsResolutionsFiller(const CSetting *setting, 
     std::vector<RESOLUTION_WHR> resolutions = g_Windowing.ScreenResolutions(info.iScreen, info.fRefreshRate);
     for (std::vector<RESOLUTION_WHR>::const_iterator resolution = resolutions.begin(); resolution != resolutions.end(); ++resolution)
     {
+if (resolution->ResInfo_Index == RES_DESKTOP)
+      list.push_back(std::make_pair(StringUtils::Format("DESKTOP"), resolution->ResInfo_Index));
+else
       list.push_back(std::make_pair(
         StringUtils::Format("%dx%d%s", resolution->width, resolution->height,
                             ModeFlagsToString(resolution->flags, false).c_str()),
