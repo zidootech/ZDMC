4f18e73b8a8f535b0dfaef4bdd0247442d60fa25
popcornmix <popcornmix@gmail.com>
2016-01-12 16:29:57 +0000

    ffmpeg: Add cabac opimisations for hevc

--- tools/depends/target/ffmpeg/autobuild.sh
+++ tools/depends/target/ffmpeg/autobuild.sh
@@ -131,6 +131,7 @@ patch -p1 < ../0001-mpeg4video-Signal-unsupported-GMC-with-more-than-one.patch
 patch -p1 < ../hevcdsp_ARM_NEON_optimized_epel_functions.patch
 patch -p1 < ../added_ARM_NEON_optimized_SAO_patches.patch
 patch -p1 < ../pfcd_hevc_optimisations.patch
+patch -p1 < ../0001-Squashed-commit-of-the-following.patch
 
 CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS" \
 ./configure --prefix=$FFMPEG_PREFIX \
